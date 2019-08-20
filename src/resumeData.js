06043b14fdba59ef04bd2ab7c9578b3bef880773
leodaiub <leodaiub@gmail.com>
2019-08-20 01:19:51 -0300

    starting portfolio

---
+++ src/resumeData.js
@@ -0,0 +1,80 @@
+let resumeData = {
+    "imagebaseurl":"https://leodaiub.github.io/",
+    "name": "Leonardo Iacomini Daiub",
+    "role": "Desenvolvedor Web Fullstack",
+    "linkedinId":"leonardo-iacomini-daiub-258b21b2",
+    "skypeid": "leodaiub",
+    "roleDescription": "Gosto de transformar códigos em soluções para problemas. buscando sempre aprimorar meus conhecimentos nas tecnologias que utilizo e estar sempre atualizado com as mais modernas tecnologias do mercado. ",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/leonardo-iacomini-daiub-258b21b2/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/leodaiub",
+          "className":"fa fa-github"
+        },
+      ],
+    "aboutme":"Gosto de transformar códigos em soluções para problemas. buscando sempre aprimorar meus conhecimentos nas tecnologias que utilizo e estar sempre atualizado com as mais modernas tecnologias do mercado. ",
+    "address":"Brasil",
+    "website":"https://leonardodaiub.com",
+    "education":[
+    ],
+    "work":[
+      {
+        "CompanyName":"iTFUTURE - Consultoria de CRM",
+        "specialization":"Analista e Desenvolvedor de Software",
+        "MonthOfLeaving":"Fev",
+        "YearOfLeaving":"2019 - Até o Momento",
+        "Achievements":"- Desenvolvimento em PHP         - Desenvolvimento em JavaScript         - Desenvolvimento de API's         - Desenvolvimento de funções e integrações para SugarCRM e ZohoCRM "
+      },
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"Node.js"
+      },
+      {
+        "skillname":"MongoDB"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
